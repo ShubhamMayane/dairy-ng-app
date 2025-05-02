240949efb1044af625f042ac3b3f829863299217
ShubhamMayane <shubhammayane@gmail.com>
2025-05-03 03:19:49 +0530

    working-on-navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,44 @@ import { HeaderContactDetailsComponent } from './modules/shared/components/heade
 import { NavbarComponent } from "./modules/shared/components/navbar/navbar.component";
 import { FooterComponent } from "./modules/shared/components/footer/footer.component";
 import { BannerComponent } from './modules/shared/components/banner/banner.component';
+import { HostListener, OnInit } from '@angular/core';
+import { MobileNavbarComponent } from "./modules/shared/components/mobile-navbar/mobile-navbar.component";
+import { CommonModule } from '@angular/common';
+
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderContactDetailsComponent, NavbarComponent, FooterComponent,BannerComponent],
+  imports: [RouterOutlet,CommonModule, HeaderContactDetailsComponent, NavbarComponent, FooterComponent, BannerComponent, MobileNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'dairy-ng-app';
+
+  isMobile: boolean = false;
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit() {
+    if (this.isBrowser) {
+      this.checkScreenSize();
+    }
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    if (this.isBrowser) {
+      this.checkScreenSize();
+    }
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 1024;
+    console.log(this.isMobile)
+
+  }
 }
---
+++ src/app/modules/shared/components/mobile-navbar/mobile-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-mobile-navbar',
+  imports: [CommonModule,RouterModule],
+  templateUrl: './mobile-navbar.component.html',
+  styleUrl: './mobile-navbar.component.css'
+})
+export class MobileNavbarComponent {
+
+}
