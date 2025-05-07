2b3862d0f6e0549ea5ed7ae584cab98305c59e8c
ShubhamMayane <shubhammayane@gmail.com>
2025-05-07 11:58:01 +0530

    finalDemo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, HostListener, OnInit } from '@angular/core';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderContactDetailsComponent } from './modules/shared/components/header-contact-details/header-contact-details.component';
 import { NavbarComponent } from "./modules/shared/components/navbar/navbar.component";
 import { FooterComponent } from "./modules/shared/components/footer/footer.component";
 import { BannerComponent } from './modules/shared/components/banner/banner.component';
-import { HostListener, OnInit } from '@angular/core';
 import { MobileNavbarComponent } from "./modules/shared/components/mobile-navbar/mobile-navbar.component";
-import { CommonModule } from '@angular/common';
 
-import { Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule, HeaderContactDetailsComponent, NavbarComponent, FooterComponent, BannerComponent, MobileNavbarComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderContactDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent,
+    MobileNavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   title = 'dairy-ng-app';
-
   isMobile: boolean = false;
   isBrowser: boolean;
 
@@ -40,8 +46,9 @@ export class AppComponent implements OnInit  {
   }
 
   checkScreenSize() {
-    this.isMobile = window.innerWidth < 1024;
-    console.log(this.isMobile)
-
+    if (this.isBrowser) {
+      this.isMobile = window.innerWidth < 1024;
+      console.log('isMobile:', this.isMobile);
+    }
   }
 }
--- src/app/modules/shared/components/mobile-navbar/mobile-navbar.component.ts
+++ src/app/modules/shared/components/mobile-navbar/mobile-navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+
+declare var bootstrap: any; // Needed if using Bootstrap 5 and no types
 @Component({
   selector: 'app-mobile-navbar',
   imports: [CommonModule,RouterModule],
@@ -9,4 +12,21 @@ import { RouterModule } from '@angular/router';
 })
 export class MobileNavbarComponent {
 
+  isNavbarOpen = false;
+
+  closeNavbar() {
+    // alert("clicked");
+    const navbar = document.getElementById('navbarSupportedContent');
+    if (navbar?.classList.contains('show')) {
+      const bsCollapse = bootstrap.Collapse.getInstance(navbar) || new bootstrap.Collapse(navbar, { toggle: false });
+      bsCollapse.hide();
+      this.isNavbarOpen = !this.isNavbarOpen;
+    }
+  }
+
+
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
 }
