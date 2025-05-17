f2fcb85dcde81c80d046326a8a7d7e552c0eed7c
ShubhamMayane <shubhammayane@gmail.com>
2025-05-17 21:26:06 +0530

    update-app-routing-file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    {
-        path: 'home',
-        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-      },
-      {
+     {
         path:'',
         redirectTo: 'home',
         pathMatch: 'full'
-      },
+      },  
+      {
+          path: 'home',
+          loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+        },
       {
         path: 'about-us',
         loadChildren: () => import('./modules/aboutus/aboutus.module').then(m => m.AboutusModule)
