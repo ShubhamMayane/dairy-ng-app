343cb15169cd97308528b0f5f50cc3feb93386b3
ShubhamMayane <shubhammayane@gmail.com>
2025-06-04 18:07:38 +0530

    04-06-2025

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {   ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+ import{AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,7 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent  implements AfterViewInit  {
   workingTime:string="Mon - Thu 9:00 AM - 7:00 PM";
   facebookLink:string="https://www.facebook.com/DKDairyfarmpune/";
   instagramLink:string="https://www.instagram.com/dkdairyfarmpune/";
@@ -20,34 +23,47 @@ export class FooterComponent {
     emailId:string="sagardike92@gmail.com";
     phoneNumber:string="+91 8793760700 ";
     farmAddress:string="At Post Ambegaon Khed Manchar,Bhimashankar Karkhana Road, Manchar-01,Pune 411051";
+    officeAddress:string="Opp gitanjalli bungalow biside PMC school In Lane prassndada jagtap office, Canal Rd, Hingane Khurd, Hingne Khurd, Pune, Maharashtra 411051";
+
+  @ViewChild('shakeTarget') shakeTarget!: ElementRef;
+  isVisible = false;
 
 
-  subscribeForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.subscribeForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  //1
+    ngAfterViewInit() 
+    {
+
+          if (isPlatformBrowser(this.platformId) && 'IntersectionObserver' in window) 
+          {
+                    const observer = new IntersectionObserver(
+                      ([entry]) => {
+                        if (entry.isIntersecting && !this.isVisible) {
+                          this.isVisible = true; // Trigger animation
+                        }
+                      },
+                      { threshold: 0.6 } // Trigger when 60% visible
+                    );
+
+                    observer.observe(this.shakeTarget.nativeElement);
+          }
+    }
+
+
+//2 scrol to op
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scroll
     });
   }
 
 
-  get email() {
-    return this.subscribeForm.get('email')!;
-  }
+
     
-  onSubmit() {
-    if (this.subscribeForm.valid) {
-      const email = this.email.value;
-      console.log('Subscribed with email:', email);
-      alert(`Subscribed successfully with: ${email}`);
-
-      // logic to reset all form 
-      this.subscribeForm.reset();
-    } else 
-    {
-      this.subscribeForm.markAllAsTouched();
-    }
 
 
 }
-}
+
--- src/app/modules/shared/components/photo-gallery/photo-gallery.component.ts
+++ src/app/modules/shared/components/photo-gallery/photo-gallery.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-photo-our-gallery',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './photo-gallery.component.html',
   styleUrl: './photo-gallery.component.css'
 })
-export class PhotoGallaryComponent {
+export class PhotoGallaryComponent 
+{
+    selectedIndex: number | null = null;
+    images: string[] = 
+              [
+                'images/gallery-images/gallery-img-left-1.jpg',
+                'images/gallery-images/gallery-img-left-2.jpg',
+                'images/gallery-images/gallery-img-left-3.jpg',
+                'images/gallery-images/gallery-img-right-1.jpg',
+                'images/gallery-images/gallery-img-right-2.jpg',
+                'images/gallery-images/gallery-img-right-3.jpg'
+               
+               ];
 
+ //1.to open image viewer
+            openViewer(index: number) 
+            {
+              console.log(index);
+              
+                this.selectedIndex = index;
+            }
+
+
+            //2.to close opened image viewer
+            closeViewer() 
+            {
+                this.selectedIndex = null;
+            }
+
+
+            //3.to go to prev image
+            prevImage() 
+            {
+                if (this.selectedIndex !== null) {
+                this.selectedIndex = (this.selectedIndex - 1 + this.images.length) % this.images.length;
+                }
+            }
+
+            //4.to go to next image
+            nextImage() {
+                if (this.selectedIndex !== null) {
+                this.selectedIndex = (this.selectedIndex + 1) % this.images.length;
+                }
+            }
 }
